5a0d0e62adc9972fb0627ddbfdca346c75ce5486
Jacques KAISER <jackokaiser@gmail.com>
2022-12-29 16:46:29 +0100

    adapted to new takeout location interface

--- scripts/plot_locations.py
+++ scripts/plot_locations.py
@@ -5,6 +5,8 @@ import shutil
 import tempfile
 from bisect import bisect_left
 from datetime import datetime
+from dateutil.parser import isoparse
+from pytz import timezone
 import pandas as pd
 from geopy import distance
 import numpy as np
@@ -22,12 +24,13 @@ class Location:
 
     @classmethod
     def deserialize(cls, loc: dict):
-        return cls(**{
-            'latitude': float(loc['latitudeE7']) / 1e7,
-            'longitude': float(loc['longitudeE7']) / 1e7,
-            'timestamp': int(loc['timestampMs']) // 1000,
-            'accuracy': loc['accuracy']
-        })
+        return cls(
+            **{
+                'latitude': float(loc['latitudeE7']) / 1e7,
+                'longitude': float(loc['longitudeE7']) / 1e7,
+                'timestamp': int(get_date(loc).timestamp()),
+                'accuracy': loc['accuracy']
+            })
 
     @property
     def latlon(self) -> tuple:
@@ -72,20 +75,30 @@ def to_kml(locations: list, save_path: str) -> None:
 
 def load_location_history(path_to_location_zip: str) -> dict:
     with tempfile.TemporaryDirectory() as tmp_dir:
-        # unzip location history to temporary directory
         shutil.unpack_archive(path_to_location_zip, tmp_dir, 'zip')
-        location_history_path = os.path.join(tmp_dir, 'Takeout/Location History/Location History.json')
-        location_history = load_json(location_history_path)
-        return location_history
+        location_history_rel_paths = [
+            'Takeout/Location History/Location History.json', 'Takeout/Location History/Records.json'
+        ]
+        for location_history_rel_path in location_history_rel_paths:
+            location_history_path = os.path.join(tmp_dir, location_history_rel_path)
+            if os.path.isfile(location_history_path):
+                return load_json(location_history_path)
+        raise FileNotFoundError(f'Did not find location history in {path_to_location_zip}')
 
 
 def get_date(location: dict) -> datetime:
-    return datetime.fromtimestamp(int(location['timestampMs']) / 1000)
+    if 'timestampMs' in location:
+        return datetime.fromtimestamp(int(location['timestampMs']) / 1000)
+    elif 'timestamp' in location:
+        paris_timezone = timezone('Europe/Paris')
+        return isoparse(location['timestamp']).astimezone(paris_timezone).replace(tzinfo=None)
+    else:
+        raise RuntimeError(f'Did not find timestamp in {location.keys()}')
 
 
 def subsample_locations(locations: list, min_km_thresh: float = 10, outlier_km_thresh: float = 50) -> list:
-    distances = np.array([distance.distance(loc_a.latlon, loc_b.latlon).km
-                          for loc_a, loc_b in zip(locations[:-1], locations[1:])])
+    distances = np.array(
+        [distance.distance(loc_a.latlon, loc_b.latlon).km for loc_a, loc_b in zip(locations[:-1], locations[1:])])
 
     def is_outlier(ii: int) -> bool:
         return distances[ii] > outlier_km_thresh
@@ -98,8 +111,7 @@ def subsample_locations(locations: list, min_km_thresh: float = 10, outlier_km_t
     return subsampled_locations
 
 
-def extract_locations_in_interval(locations: list,
-                                  start_date: datetime, stop_date: datetime) -> list:
+def extract_locations_in_interval(locations: list, start_date: datetime, stop_date: datetime) -> list:
     start_idx = bisect_left(locations, start_date, key=lambda location: get_date(location))
     stop_idx = bisect_left(locations, stop_date, key=lambda location: get_date(location))
     return locations[start_idx:stop_idx]
@@ -115,6 +127,7 @@ def plot_locations(path_to_location_zip: str, date_interval: list, save_dir: str
     locations = extract_locations_in_interval(location_history['locations'], start_date, stop_date)
     locations = [Location.deserialize(loc) for loc in locations]
     locations = subsample_locations(locations)
+    os.makedirs(save_dir, exist_ok=True)
     convert_to_df(locations).to_csv(os.path.join(save_dir, 'locations.csv'), index=False)
     to_kml(locations, os.path.join(save_dir, 'locations.kml'))
 
